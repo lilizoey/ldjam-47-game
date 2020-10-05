27783d50f1fff56c1b8adb34bb6a49390c8b3a0c
Lili Andersen <lili.andersen@nrk.no>
2020-10-05 19:17:54 +0200

    Add turret stuff

--- Hazards/Turret/Turret.gd
+++ Hazards/Turret/Turret.gd
@@ -1,14 +1,38 @@
 extends Node2D
 
-
+export var rotation_speed_degrees: float = 60
 export var player_path: NodePath
-onready var player: KinematicBody2D  
+export var fire_delay: float = 1.9
+export var charge_time: float = 2
+export var firing_time: float = 0.1
+export var firing_angle_degrees: float = 10
+
+var player: KinematicBody2D  
+
+var rotation_speed: float = deg2rad(rotation_speed_degrees)
+var firing_angle: float = deg2rad(firing_angle_degrees)
+
+var can_fire = true
 
 func _ready():
 	call_deferred("init_player")
 
+func _process(delta):
+	if not player:
+		init_player()
+
 func init_player():
-	player = get_parent().get_player()
+	player = get_parent().get_parent().get_parent().get_player()
+
+func rotate_towards(delta, pos: Vector2):
+	var target_angle = global_position.angle_to_point(pos)
+	if target_angle > $Gun.rotation:
+		$Gun.rotation += rotation_speed * delta
+	if target_angle < $Gun.rotation:
+		$Gun.rotation -= rotation_speed * delta
+	
+	return abs(target_angle - $Gun.rotation) < firing_angle
+
 
 func _physics_process(delta):
 	if player:
@@ -16,7 +40,18 @@ func _physics_process(delta):
 		var result = space_state.intersect_ray(global_position, player.global_position, [$Base])
 	
 		if result and result.collider == player:
-			$Gun/Sprite.look_at(player.global_position)
-			$Gun/Sprite.rotate(PI)
-	
+			var in_range = rotate_towards(delta, player.global_position)
+			if in_range and can_fire:
+				fire()
+
+func fire():
+	can_fire = false
+	$ChargeUpSound.play()
+	yield(get_tree().create_timer(charge_time), "timeout")
+	print("poof")
+	$Gun/FiringLazer.visible = true
+	yield(get_tree().create_timer(firing_time), "timeout")
+	$Gun/FiringLazer.visible = false
+	yield(get_tree().create_timer(fire_delay), "timeout")
+	can_fire = true
 	
